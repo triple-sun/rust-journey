7084da92b14c36255f649ed3187f38d7d61a541b
Simeon <sploit.strannik@gmail.com>
2026-03-05 23:46:10 +0300

    added eprintln

--- exercises/block_5/minigrep/src/main.rs
+++ exercises/block_5/minigrep/src/main.rs
@@ -5,14 +5,14 @@ use minigrep::{Config, search};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Error while parsing arguments: {err}");
+        eprintln!("Error while parsing arguments: {err}");
         process::exit(1);
     });
 
     println!("Looking for {} in {}", config.query, config.filepath);
 
     if let Err(e) = run(&config) {
-        print!("An error has occured: {e}");
+        eprintln!("An error has occured: {e}");
         process::exit(1);
     }
 }
